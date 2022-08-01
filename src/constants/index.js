69c19e93807255aa71a3055f30fe76fceedebeb2
hnhtran <tran_eetas@yahoo.com>
2022-08-01 23:56:52 +1000

    deploy constants error

---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+export const API_URL = "http://localhost:8000/api/watches/";
+export const USER_URL = "http://localhost:8000/api/users/dj-rest-auth/";
+
+// heroku config:set USER_URL='https://trasia-watches-store.herokuapp.com/api/users/dj-rest-auth/'
